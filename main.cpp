4342e0c1b71cc4d04fd8586ce576f9d31e92ae48
Erick Martinez <erickmartinez0131@gmail.com>
2025-12-30 14:18:59 -0500

    more boilerplate

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
  *
  */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -20,10 +21,36 @@ struct YearlySnapshot {
 
 void runSimulation(double principal, double annualCont, double rate,
                    double taxRate) {
+    double taxableBal = principal;
+    double taxFreeBal = principal;
+    rate = rate / 100;
+    taxRate = taxRate / 100;
+
+    ofstream file("simulation_results.csv");
+    file
+        << "Year,Taxable Balance, Tax-advantaged Balance, Wealth Lost to Tax\n";
+
+    for (int i = 1; i <= 10; ++i) {
+        // Tax-Advantaged Growth
+        taxFreeBal += annualCont;
+        taxFreeBal *= (1 + rate); // effective return: r(1 - T)
+
+        // Taxable Growth
+        taxableBal += annualCont;
+        double interest = taxableBal * rate;
+        double taxDebt = interest * taxRate;
+        taxableBal += (interest - taxDebt);
+
+        file << i << "," << taxableBal << "," << taxFreeBal << ","
+             << (taxFreeBal - taxableBal) << endl;
+    }
+    file.close();
+    cout << "Simulation complete. Results exported to simulation_results.csv"
+         << endl;
 }
 
 int main() {
-    double principal, annualCont, rate, taxRate;
+    double principal = 0.0, annualCont = 0.0, rate = 0.0, taxRate = 0.0;
     runSimulation(principal, annualCont, rate, taxRate);
     return 0;
 }
